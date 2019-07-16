1c5c32104053bc0dc429ef11fc1cc3712ed0f230
Alex-Weatherhead <22604743+Alex-Weatherhead@users.noreply.github.com>
2019-07-16 09:22:38 -0400

    Add attribution for map tiles.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var markers = [];
 const GLOBAL_MARKER_TYPE = "G";
 const LOCAL_MARKER_TYPE = "L";
 
-L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png ', {
+L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png ', { // Attribution: Wikimedia Maps | Map data OpenStreetMap contributors
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     continuousWorld: true
 }).addTo(map);
